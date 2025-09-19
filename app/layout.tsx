ea997f5f946959d29c9e5915a5b0f784ea1a6801
Jovan <jvb10x@outlook.com>
2025-09-18 21:30:54 -0500

    update dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,47 +1,47 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import { ThemeProvider } from '@/components/theme-provider';
 
-import Provider from "@/context/Theme";
-import "./globals.css";
+import './globals.css';
 
 const inter = localFont({
-  src: "./fonts/InterVF.ttf",
-  variable: "--font-inter",
-  weight: "100 200 300 400 500 700 800 900",
+	src: './fonts/InterVF.ttf',
+	variable: '--font-inter',
+	weight: '100 200 300 400 500 700 800 900',
 });
 
 const spaceGrotesk = localFont({
-  src: "./fonts/SpaceGroteskVF.ttf",
-  variable: "--font-space-grotesk",
-  weight: "300 400 500 700",
+	src: './fonts/SpaceGroteskVF.ttf',
+	variable: '--font-space-grotesk',
+	weight: '300 400 500 700',
 });
 
 export const metadata: Metadata = {
-  title: "DevFlow",
-  description:
-    "A community-driven platform. for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, alogorithms, and structures, and more.",
-  icons: {
-    icon: "/images/site-logo.svg",
-  },
+	title: 'DevFlow',
+	description:
+		'A community-driven platform. for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, alogorithms, and structures, and more.',
+	icons: {
+		icon: '/images/site-logo.svg',
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>
-        <Provider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange>
-          {children}
-        </Provider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en' suppressHydrationWarning>
+			<body
+				className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
